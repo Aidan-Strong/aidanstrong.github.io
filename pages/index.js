b07265a539a1f5032e696b77683d30106ab2f0c9
Aidan Strong <aistrong@Aidans-MBP.attlocal.net>
2021-02-23 16:17:54 -0800

    Added github actions

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,35 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Aidan Strong</title>
+        <link rel="icon" href="/icon.png" />
       </Head>
 
       <main className={styles.main}>
+        <img className={styles.img} src="/headshot2.png"></img>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello, I'm  <span style={{ color: "blue" }}>Aidan Strong</span>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Pure Mathematics Major & Gender Studies minor at UCLA
         </p>
 
+        {/* table stuff */}
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <a href='/pages/aboutme' className={styles.card}>About Me</a>
+          <a href='/resume.pdf' className={styles.card}>Resume</a>
+          <a href='https://str0nkyk0ng.itch.io/' className={styles.card}>Game Development</a>
+          <a href='mailto: aidan.m.strong@gmail.com' className={styles.card}>Contact</a>
         </div>
+
+        <footer className={styles.footer}>
+          &copy; Aidan Strong 2021
+      </footer>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+
+
+    </div >
   )
 }
---
+++ pages/pages/aboutme.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+export default function Projects() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>About Me</title>
+                <link rel="icon" href="/icon.png" />
+            </Head>
+
+            <main className={styles.aboutme}>
+
+                <h1 className={styles.title}>
+                    <span style={{ color: "blue" }}>About Me</span>
+                </h1>
+
+            </main>
+            <div className={styles.verticalBar}></div>
+            <p className={styles.aboutMeParagraph}>Hi, I'm Aidan Strong! I'm a second year at UCLA majoring in Pure Mathematics (with a concentration in computing),
+            and minoring in Gender Studies.
+                <br></br>
+                <br></br>
+                When I'm not completing PSets, I like to <a href="https://store.steampowered.com/app/1460290/Loop/" style={{ color: "blue" }}>program video games</a>,
+                <a href="https://www.facebook.com/UclaSmaugUltimate/" style={{ color: "blue" }}> play ultimate frisbee</a>, and preform. I'm interested in research and work regarding computer graphics, game engines, and game theory.
+                </p>
+
+            <footer className={styles.footer}>
+                &copy; Aidan Strong 2021
+        </footer>
+
+        </div >
+    )
+} ``
